14d25dbecb9559bbe691ae693059c63dad1d16e1
Mandever <evelyn.amanda14@gmail.com>
2020-03-14 22:58:51 +0100

    Fase de registro funciona

--- src/bot/manager.py
+++ src/bot/manager.py
@@ -1,9 +1,9 @@
 from bot.parameters import Parameters
-from telegram import Bot, Update
-from telegram.ext import Dispatcher, CommandHandler, Updater, CallbackQueryHandler, ConversationHandler, MessageHandler, Filters, ReplyKeyboardMarkup
+from telegram import Bot, Update, ReplyKeyboardMarkup
+from telegram.ext import Dispatcher, CommandHandler, Updater, CallbackQueryHandler, ConversationHandler, MessageHandler, Filters
 from logger_handler import get_logger
 from bot.commands import start, button, help_command, error
-import json
+import json, logging
 
 class Bot_Manager:
     def __init__(self, logger):
@@ -39,7 +39,7 @@ class Bot_Manager:
         self.dispatcher.add_handler(start_handler)
         self.dispatcher.add_handler(CallbackQueryHandler(button))
         self.dispatcher.add_error_handler(error)
-        self.dispatcher.add_handler(setup_registrar_handler)
+        self.dispatcher.add_handler(setup_registrar_handler())
 
     def start_bot(self):
         """
@@ -54,7 +54,7 @@ class Bot_Manager:
 CHOOSING, TYPING_CHOICE = range(2)
 
 def setup_registrar_handler():
-    CHOOSING, TYPING_REPLY = range(2)
+    ##CHOOSING, TYPING_REPLY = range(2)
     return ConversationHandler(
         entry_points=[CommandHandler('registrar', start_registrar)],
 
@@ -63,20 +63,20 @@ def setup_registrar_handler():
             TYPING_CHOICE: [MessageHandler(Filters.text, received_information)]
         },
 
-        fallbacks=[MessageHandler(Filters.regex('^Done$'), done)]
+        fallbacks=[MessageHandler(Filters.regex('^SI$'), done)]
     )
 
 reply_keyboard = [
     ['Nombre completo', 'Mes de pago'],
     ['Grupo de trabajo'],
-    ['Done']
+    ['SI']
 ]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
 def start_registrar(update, context):
     update.message.reply_text(
-        "Hola, soy Wallee, el bot del FABLAB Badajoz"
-        "¿En que te puedo ayudar?",
+        "Hola, soy Wallee, el bot del FABLAB Badajoz "
+        "Vamos a proceder a registrarte Ok?",
         reply_markup=markup)
 
     return CHOOSING
@@ -85,19 +85,63 @@ def generic_choice(update, context):
     text = update.message.text
     context.user_data['choice'] = text
     if (text == 'Nombre completo'):
-        update.message.reply_text('Escriba su nombre completo a continuación')
+        update.message.reply_text('Escriba su nombre completo a continuación:')
     elif (text == 'Mes de pago'):
-        update.message.reply_text('Escriba el mes de pago de la cuota anual')    
+        update.message.reply_text('Escriba el mes de pago de la cuota anual:')    
     elif (text == 'Grupo de trabajo'):
-        update.message.reply_text('Escriba sus grupos de trabajo separados por coma')
+        update.message.reply_text('Escriba su grupo u grupos de trabajo separados por coma:')
 
     return TYPING_CHOICE
 
 def received_information(update, context):
-    pass
+    user_data = context.user_data
+    print(user_data)
+    text = update.message.text
+    print(text)
+    category = user_data['choice']
+    print(category)
+    user_data[category] = text
+    
+    #update.message.reply_text("Bien!! Por ahora tenemos esto:"
+    #                            "{} .Continuemos..." .format(facts_to_str(user_data)),
+    #                            reply_markup=markup)
+    
+    if (category == 'Nombre completo'):
+        update.message.reply_text('Bien!! Su nombre es: {}. \n'
+                                    'Ahora continua indicando cuando fue el pago de la cuota '
+                                    'anual.'.format(text),
+                                    reply_markup=markup)
+    elif (category == 'Mes de pago'):
+        update.message.reply_text('Genial!! El mes de pago de tu cuota fue en: {}. \n'
+                                    'Por ultimo continua a introducir a que grupo de trabajo '
+                                    'pertenece.' .format(text),
+                                    reply_markup=markup)    
+    elif (category == 'Grupo de trabajo'):
+        update.message.reply_text('Perfecto!! Tus datos introducidos son los siguientes: '
+                                    '{}.\n ¿Esta todo correcto?' .format(facts_to_str(user_data)),
+                                    reply_markup=markup)
+        
+    del user_data['choice']
+
+    return CHOOSING
+    
 
 def done(update, context):
-    pass
+    user_data = context.user_data
+    if 'choice' in user_data:
+        del user_data['choice']
+
+    update.message.reply_text("Felicidades!! El registro se ha completado correctamente...")
+
+    user_data.clear()
+    return ConversationHandler.END
+
+
+def facts_to_str(user_data):
+    facts = list()
+    for key, value in user_data.items():
+        facts.append('{} - {}'.format(key, value))
+    return "\n".join(facts).join(['\n', '\n'])
 
     # Old Code
     # def set_webhook(self):
